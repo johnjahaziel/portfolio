36126a2a1e0cf8883386f99ab0b34a757d756d6f
johnjahaziel <johnjahaziel00@gmail.com>
2025-08-19 15:53:41 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import AboutPage from "./components/AboutPage";
 import HeroSection from "./components/Herosection";
+import TechnicalJourney from "./components/TechnicalJourney";
 
 const App = () => {
   return (
     <div style={{ background: "black" }}>
       <HeroSection />
       <AboutPage />
+      <TechnicalJourney />
       {/* You can keep adding more sections */}
     </div>
   );
---
+++ src/components/TechnicalJourney.jsx
@@ -0,0 +1,12 @@
+import "../css/technicaljourney.css";
+
+const TechnicalJourney = () => {
+    return (
+        <div className="technical-journey">
+            <h1 className="tj-title">MY</h1>
+            <h3 className="tj-subtitle">DEVELOPER JOURNEY</h3>
+        </div>
+    );
+}
+
+export default TechnicalJourney;
