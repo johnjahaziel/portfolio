400bc4c708065debb0862d5480c4587b81dc5340
johnjahaziel <johnjahaziel00@gmail.com>
2025-08-22 15:39:00 +0530

    herosection

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import AboutPage from "./components/AboutPage";
 import Contact from "./components/Contact";
 import EducationPath from "./components/EducationPath";
 import Experience from "./components/Experience";
-import HeroSection from "./components/Herosection";
+import HeroSection from "./components/HeroSection";
 import Projects from "./components/Projects";
 import TechnicalJourney from "./components/TechnicalJourney";
 import Techstack from "./components/Techstack";
