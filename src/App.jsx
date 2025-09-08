f48538910fccffca32e33efce1ea79411afecc35
johnjahaziel <johnjahaziel00@gmail.com>
2025-09-08 18:06:32 +0530

    seo

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import AboutPage from "./components/AboutPage";
 import Contact from "./components/Contact";
 import EducationPath from "./components/EducationPath";
@@ -9,17 +10,31 @@ import Techstack from "./components/Techstack";
 
 const App = () => {
   return (
-    <div style={{ background: "black", position: "relative", zIndex: 0 }}>
-      <HeroSection />
-      <AboutPage />
-      <TechnicalJourney />
-      <EducationPath />
-      <Experience />
-      <Techstack />
-      <Projects />
-      <Contact />
-      {/* You can keep adding more sections */}
-    </div>
+    <HelmetProvider>
+      <div style={{ background: "black", position: "relative", zIndex: 0 }}>
+        {/* SEO Meta Tags */}
+        <Helmet>
+          <title>John Jahaziel | Portfolio</title>
+          <meta
+            name="description"
+            content="Official portfolio of John Jahaziel, Flutter Developer. Explore my projects, experience, and journey."
+          />
+          <meta name="keywords" content="John Jahaziel, Portfolio, Flutter Developer" />
+          <meta name="author" content="John Jahaziel" />
+          <link rel="canonical" href="https://johnjahaziel.vercel.app/" />
+        </Helmet>
+
+        {/* Sections */}
+        <HeroSection />
+        <AboutPage />
+        <TechnicalJourney />
+        <EducationPath />
+        <Experience />
+        <Techstack />
+        <Projects />
+        <Contact />
+      </div>
+    </HelmetProvider>
   );
 };
 
